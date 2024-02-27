077f5f5ebb22fa49af804c0c307b112da64c0590
Ana Oliveira <151927439+estudosdetidaana98@users.noreply.github.com>
2024-02-27 04:47:20 -0300

    Versão Atualizada - Componentes e a Models

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,36 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+
+function Footer() {
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  let footerComponent
+
+  let data = new Date().getFullYear()
+
+  if(usuario.token !== '') {
+    footerComponent = (
+      <>
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <div className='flex gap-2'>
+              <LinkedinLogo size={30} weight='bold' />
+              <InstagramLogo size={30} weight='bold' />
+              <FacebookLogo size={30} weight='bold' />
+            </div>
+          </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      {footerComponent}
+    </>
+  )
+}
+
+export default Footer
